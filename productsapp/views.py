3f1f2423414b52d6926fa846e7b0f71301dad05a
Bread <primebox21@gmail.com>
2022-07-11 13:29:30 +0700

    added passing context to a templates for productsapp

--- productsapp/views.py
+++ productsapp/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 def index(request):
-    return render(request, 'productsapp/index.html')
+    context = {
+        'title': 'Chop'
+    }
+    return render(request, 'productsapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'productsapp/products.html')
+    context = {
+        'title': 'Chop - Каталог'
+    }
+    return render(request, 'productsapp/products.html', context)
