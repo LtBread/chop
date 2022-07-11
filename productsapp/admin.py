fce6a1e8f333ae01078b44fe62a97d6f4400a3a1
Bread <primebox21@gmail.com>
2022-07-11 18:43:07 +0700

    registered ProductCategory and Product on productsapp/admin.py, set up media connection

--- productsapp/admin.py
+++ productsapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from productsapp.models import ProductCategory, Product
 
 # Register your models here.
+
+
+admin.site.register(ProductCategory)
+admin.site.register(Product)
--- productsapp/models.py
+++ productsapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='имя категории', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание категории', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products_images', blank=True, null=True)
+    name = models.CharField(verbose_name='имя продукта', max_length=256)
+    price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
+    description = models.TextField(verbose_name='описание продукта', blank=True)
+    quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
+    is_active = models.BooleanField(verbose_name='активно', default=True)
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
+
+
+
