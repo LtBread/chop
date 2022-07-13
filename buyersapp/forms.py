f409420365d191c0c668cd44a5c1399154dcd98c
Bread <primebox21@gmail.com>
2022-07-13 18:01:49 +0700

    created buyersapp/forms.py, added login and logout by buyersapp/views.py

---
+++ buyersapp/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import AuthenticationForm
+
+from buyersapp.models import Buyer
+
+
+class BuyerLoginForm(AuthenticationForm):
+    class Meta:
+        model = Buyer
+        fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(BuyerLoginForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
--- buyersapp/views.py
+++ buyersapp/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
 from buyersapp.models import Buyer
+from buyersapp.forms import BuyerLoginForm
 
 
 # Create your views here.
 
 def login(request):
-    pass
+    form = BuyerLoginForm(data=request.POST)
+    if request.method == 'POST' and form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active():
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main'))
+
+    context = {
+        'title': 'Chop - Авторизация',
+        'form': form,
+    }
+
+    return render(request, 'buyersapp/login.html', context)
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main'))
