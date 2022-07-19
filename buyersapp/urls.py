58a260186a955aa0b567fba77b70120d9dc02b05
Bread <primebox21@gmail.com>
2022-07-19 20:02:01 +0700

    created profile template

--- buyersapp/urls.py
+++ buyersapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from buyersapp.views import login, registration, logout
+from buyersapp.views import login, registration, logout, profile
 
 app_name = 'buyersapp'
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('registration/', registration, name='registration'),
+    path('profile/', profile, name='profile'),
     path('logout/', logout, name='logout'),
 ]
--- buyersapp/views.py
+++ buyersapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
 from buyersapp.models import Buyer
@@ -30,12 +30,18 @@ def registration(request):
         form = BuyerRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы успешно зарегистрировались!')
             return HttpResponseRedirect(reverse('buyers:login'))
     else:
         form = BuyerRegistrationForm()
 
     context = {'title': 'Chop - Регистрация', 'form': form}
     return render(request, 'buyersapp/registration.html', context)
+
+
+def profile(request):
+    context = {'title': 'Chop - Профиль'}
+    return render(request, 'buyersapp/profile.html', context)
 
 
 def logout(request):
