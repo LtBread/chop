1f0b426e11d2e4850f6c709fc2e3faf92b8ee357
Bread <primebox21@gmail.com>
2022-08-16 16:27:33 +0700

    added user_passes_test decorator

--- adminsapp/views.py
+++ adminsapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
 
 from buyersapp.models import Buyer
 from adminsapp.forms import BuyerAdminRegistrationForm, BuyersAdminProfileForm
@@ -8,12 +9,13 @@ from adminsapp.forms import BuyerAdminRegistrationForm, BuyersAdminProfileForm
 
 # Create your views here.
 
-
+@user_passes_test(lambda u: u.is_staff)
 def index(request):
     context = {'title': 'Chop - Админ-панель'}
     return render(request, 'adminsapp/index.html', context)
 
 
+@user_passes_test(lambda u: u.is_staff)
 def admin_buyers_create(request):
     if request.method == 'POST':
         form = BuyerAdminRegistrationForm(data=request.POST, files=request.FILES)
@@ -30,6 +32,7 @@ def admin_buyers_create(request):
     return render(request, 'adminsapp/admin-buyers-create.html', context)
 
 
+@user_passes_test(lambda u: u.is_staff)
 def admin_buyers_read(request):
     context = {
         'title': 'Chop - Покупатели',
@@ -38,6 +41,7 @@ def admin_buyers_read(request):
     return render(request, 'adminsapp/admin-buyers-read.html', context)
 
 
+@user_passes_test(lambda u: u.is_staff)
 def admin_buyers_update(request, buyer_id):
     selected_buyer = Buyer.objects.get(id=buyer_id)
     if request.method == 'POST':
@@ -56,6 +60,7 @@ def admin_buyers_update(request, buyer_id):
     return render(request, 'adminsapp/admin-buyers-update-delete.html', context)
 
 
+@user_passes_test(lambda u: u.is_staff)
 def admin_change_activity(request, buyer_id):
     buyer = Buyer.objects.get(id=buyer_id)
     buyer.change_activity()
