319b62b2b1b5e7ce7afadcb5fe79feecb28cb790
Bread <primebox21@gmail.com>
2022-08-18 18:53:36 +0700

    added pagination for products.html

--- productsapp/urls.py
+++ productsapp/urls.py
@@ -6,5 +6,6 @@ app_name = 'productsapp'
 
 urlpatterns = [
     path('', products, name='index'),
-    path('<int:category_id>/', products, name='category')
+    path('<int:category_id>/', products, name='category'),
+    path('page/<int:page>/', products, name='page')
 ]
--- productsapp/views.py
+++ productsapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from productsapp.models import ProductCategory, Product
 
@@ -10,12 +11,21 @@ def index(request):
     return render(request, 'productsapp/index.html', context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page=1):
     context = {
         'title': 'Chop - Каталог',
-        'categories': ProductCategory.objects.all,
-        'products': Product.objects.all()
+        'categories': ProductCategory.objects.all()
     }
     if category_id:
-        context['products'] = Product.objects.filter(category_id=category_id)
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context['products'] = products_paginator
     return render(request, 'productsapp/products.html', context)
