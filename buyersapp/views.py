ca48a480b86973592f38ce6d459e4c6ce7031378
Ilya <primebox21@gmail.com>
2022-08-23 11:01:14 +0700

    deploy project to altermachine

--- buyersapp/views.py
+++ buyersapp/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.views.generic import View
+from django.views.generic.edit import UpdateView
+from django.utils.decorators import method_decorator
 
 from buyersapp.models import Buyer
 from basketsapp.models import Basket
 from productsapp.models import Product
 from buyersapp.forms import BuyerLoginForm, BuyerRegistrationForm, BuyersProfileForm
+
+
+# class LoginView(View):
+#     def get(self, request, *args, **kwargs):
+#         form = BuyerLoginForm()
+#         context = {
+#             'title': 'Chop - Авторизация',
+#             'form': form
+#         }
+#         return render(request, 'buyersapp/login.html', context)
+#
+#     def post(self, request, *args, **kwargs):
+#         form = BuyerLoginForm(data=request.POST)
+#         if form.is_valid():
+#             username = request.POST['username']
+#             password = request.POST['password']
+#             user = auth.authenticate(username, password=password)
+#             if user and user.is_active:
+#                 auth.login(request, user)
+#                 return HttpResponseRedirect(reverse('index'))
 
 
 def login(request):
@@ -26,6 +49,23 @@ def login(request):
     return render(request, 'buyersapp/login.html', context)
 
 
+# class RegistrationView(View):
+#     def get(self, request, *args, **kwargs):
+#         form = BuyerRegistrationForm()
+#         context = {
+#             'title': 'Chop - Регистрация',
+#             'form': form
+#         }
+#         return render(request, 'buyersapp/registration.html', context)
+#
+#     def post(self, request, *args, **kwargs):
+#         form = BuyerRegistrationForm(data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Вы успешно зарегистрировались!')
+#             return HttpResponseRedirect(reverse('buyers:login'))
+
+
 def registration(request):
     if request.method == 'POST':
         form = BuyerRegistrationForm(data=request.POST)
@@ -38,6 +78,20 @@ def registration(request):
 
     context = {'title': 'Chop - Регистрация', 'form': form}
     return render(request, 'buyersapp/registration.html', context)
+
+
+# class ProfileView(UpdateView):
+#     model = Buyer
+#     form_class = BuyersProfileForm
+#     success_url = reverse_lazy('buyers:profile')
+#     template_name = 'buyersapp/profile.html'
+#
+#     def get(self):
+#         pass
+#
+#     @method_decorator(login_required)
+#     def post(self, request, *args, **kwargs):
+#         return super().post(request, *args, **kwargs)
 
 
 @login_required
